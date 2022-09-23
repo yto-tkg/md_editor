c8f663b3965e62cf065ea495ca0287f169c84b3c
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-09-23 12:23:37 +0900

    mdプレビュー機能作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  mode: "jit",
+  purge: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+  content: [
+  ],
   theme: {
     extend: {},
   },
