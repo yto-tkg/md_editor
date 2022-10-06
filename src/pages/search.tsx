1f95e0258bb3aecb4dfb3539783abfa18204c124
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-10-06 10:38:38 +0900

    feature search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,21 +1,36 @@
+import DataList from 'components/organisms/DataList'
+import { error } from 'console'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Markdown } from 'types/data'
 import Layout from '../components/templates/Layout'
 import SearchFormContainer from '../containers/SearchFormContainer'
 
 const SearchPage: NextPage = () => {
 
-  const router = useRouter()
-  const handleSubmit = (err?: Error) => {
-    if (!err) {
-      router.push(`/search`)
+ // const router = useRouter()
+ // const handleSubmit = (err?: Error) => {
+ //   if (!err) {
+ //     router.push(`/search`)
+ //   }
+ // }
+  
+  const [data, setData] = useState<Markdown[]>([])
+ 
+  const handleSubmit = (searchData: Markdown[], error?: Error) => {
+    if (!!error) {
+      alert(error)
     }
+
+    setData(searchData.data)
   }
 
   return (
     <>
       <Layout>
         <SearchFormContainer onSubmit={handleSubmit} />
+        <DataList dataList={data} />
       </Layout>
     </>
   )
