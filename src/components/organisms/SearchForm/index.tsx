8093e4f6dee7156509b727be90eb312fce005c15
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-10-31 08:48:22 +0900

    fix delete unnecessary code

--- src/components/organisms/SearchForm/index.tsx
+++ src/components/organisms/SearchForm/index.tsx
@@ -1,9 +1,5 @@
-import { red } from '@mui/material/colors'
-import { argv0 } from 'process'
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Markdown } from 'types/data'
-import DataList from '../DataList'
 
 export type SearchFormData = {
   title?: string
@@ -46,17 +42,12 @@ const SearchForm = ({ onSearchSubmit, allDataCount, children }: SearchFormProps)
   const [searchContent, setSearchContent] = useState('')
   const [offset, setOffset] = useState(0)
   const [size, setSize] = useState(10)
-  const [paging, setPaging] = useState(0)
   const [displayOffset, setDisplayOffset] = useState(0)
   const [displayLimit, setDisplayLimit] = useState(0)
   const [isDisplayPrevBtn, setIsDisplayPrevBtn] = useState('')
   const [isDisplayNextBtn, setIsDisplayNextBtn] = useState('')
   const [displayAllCount, setDisplayAllCount] = useState(allDataCount)
 
-  const setSearchData = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setSearchContent(e.target.value)
-  }
 
   useEffect(() => {
     setDisplayAllCount(allDataCount)
@@ -88,14 +79,14 @@ const SearchForm = ({ onSearchSubmit, allDataCount, children }: SearchFormProps)
 
   const onSubmit = (data: SearchFormData) => {
     const dataOrder = data.order ?? sort.order
-   
+
     if (!!data.title && data.title !== searchContent) {
       data.offset = 0
       setOffset(0)
     }
 
     setSearchContent(data.title ?? searchContent)
-   
+
     data.title = data.title ?? searchContent
     data.sort = data.sort ?? sort.key
     data.order = dataOrder == 1 ? 'asc' : 'desc'
@@ -159,7 +150,7 @@ const SearchForm = ({ onSearchSubmit, allDataCount, children }: SearchFormProps)
         name="title"
         type="text"
         placeholder="Titleを検索"
-        style={{color: 'black'}}
+        style={{ color: 'black' }}
         className="mx-auto mb-8 block h-14 w-4/5 rounded-lg border px-5 text-2xl font-bold shadow-lg focus:outline-none"
       />
       {errors.title && (
@@ -168,16 +159,16 @@ const SearchForm = ({ onSearchSubmit, allDataCount, children }: SearchFormProps)
       <input type="submit" className="submit-post my-8 mx-auto block h-10 w-36 rounded-md font-bold hover:opacity-70" />
 
       <div className='pl-10'>
-        <span style={{fontWeight: 'bold'}}>{displayOffset} - {displayLimit} ({displayAllCount}件中)</span>
+        <span style={{ fontWeight: 'bold' }}>{displayOffset} - {displayLimit} ({displayAllCount}件中)</span>
         <span style={{ display: isDisplayPrevBtn, fontWeight: 'bold' }} className='ml-4' onClick={() => onSubmitByPaging(handleSubmit(onSubmit), -1)}>←</span>
         <span style={{ display: isDisplayNextBtn, fontWeight: 'bold' }} className='ml-4' onClick={() => onSubmitByPaging(handleSubmit(onSubmit), 1)}>→</span>
         <select onChange={(e) => onDisplaySizeChange(handleSubmit(onsubmit), e)}
-          className='submit-post ml-4 h-6 w-12 rounded-md font-bold hover:opacity-70' style={{color: 'black'}}>
+          className='submit-post ml-4 h-6 w-12 rounded-md font-bold hover:opacity-70' style={{ color: 'black' }}>
           {PAGE_SIZE.map(page_size => <option value={page_size}>{page_size}</option>)}
         </select>
       </div>
 
-      <div className="flex p-4" style={{textAlign: 'center', fontWeight: 'bold', borderBottom: '0.5px solid #b3b3b3', lineHeight: '59px'}}>
+      <div className="flex p-4" style={{ textAlign: 'center', fontWeight: 'bold', borderBottom: '0.5px solid #b3b3b3', lineHeight: '59px' }}>
         <div className="flex-none w-40 h-14" onClick={() => onSubmitBySortKey(handleSubmit(onSubmit), 'title')}>title</div>
         <div className="flex-none w-40 h-14" onClick={() => onSubmitBySortKey(handleSubmit(onSubmit), 'body')}>content</div>
         <div className="flex-none w-72 h-14" onClick={() => onSubmitBySortKey(handleSubmit(onSubmit), 'createdAt')}>register time</div>
